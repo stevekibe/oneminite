920cb852cd78c28a652c20dd368b25d24d08bcb8
stevekibe <stevekibe2345.gmail.com>
2018-09-10 04:24:21 +0300

    add the code in the html files

---
+++ app/main/views.py
@@ -0,0 +1,140 @@
+from flask import render_template, request, redirect, url_for, abort
+from . import main
+from .. import db
+from ..models import User,Pitch,Comments,PitchCategory,Votes
+
+from . forms import PitchForm, CommentForm, CategoryForm
+from flask_login import login_required,current_user
+
+
+@main.route('/')
+def index():
+    '''
+    method to the root page
+    ''' 
+    category = PitchCategory.get_categories()
+
+    title = 'Home- Welcome'
+    return render_template('index.html', title = title, categories=category)
+
+@main.route('/category/new-pitch/<int:id>', methods=['GET', 'POST'])
+@login_required
+def new_pitch(id):
+    ''' 
+    method to form a new pitch
+    '''
+    form = PitchForm()
+    category = PitchCategory.query.filter_by(id=id).first()
+
+    if category is None:
+        abort(404)
+
+    if form.validate_on_submit():
+        content = form.content.data
+        new_pitch= Pitch(content=content,category_id= category.id,user_id=current_user.id)
+        new_pitch.save_pitch()
+        return redirect(url_for('.category', id=category.id))
+
+    return render_template('new_pitch.html', pitch_form=form, category=category)
+
+@main.route('/categories/<int:id>')
+def category(id):
+    category = PitchCategory.query.get(id)
+    if category is None:
+        abort(404)
+
+    pitches=Pitch.get_pitches(id)
+    return render_template('category.html', pitches=pitches, category=category)
+
+@main.route('/add/category', methods=['GET','POST'])
+@login_required
+def new_category():
+    '''
+    method for creating new category
+    '''
+    form = CategoryForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        new_category = PitchCategory(name=name)
+        new_category.save_category()
+
+        return redirect(url_for('.index'))
+
+    title = 'New category'
+    return render_template('new_category.html', category_form = form,title=title)
+
+@main.route('/view-pitch/<int:id>', methods=['GET', 'POST'])
+@login_required
+def view_pitch(id):
+    '''
+    method to view a pitch
+    '''
+    print(id)
+    pitches = Pitch.query.get(id)
+    if pitches is None:
+        abort(404)
+    
+    comment = Comments.get_comments(id)
+    return render_template('view-pitch.html', pitches=pitches, comment=comment, category_id=id)
+
+@main.route('/write_comment/<int:id>', methods=['GET', 'POST'])
+@login_required
+def post_comment(id):
+    '''
+    method to post comments
+    '''
+    form = CommentForm()
+    title = ' Comment'
+    pitches = Pitch.query.filter_by(id=id).first()
+
+    if pitches is None:
+         abort(404)
+
+    if form.validate_on_submit():
+        opinion = form.opinion.data
+        new_comment = Comments(opinion=opinion, user_id=current_user.id, pitches_id=pitches.id)
+        new_comment.save_comment()
+        return redirect(url_for('.view_pitch', id=pitches.id))
+
+    return render_template('comment.html', comment_form=form, title=title)
+
+@main.route('/pitch/upvote/<int:id>')
+@login_required
+def upvote(id):
+    '''
+    method to vote on a comment
+    '''
+    pitch_id = Pitch.query.filter_by(id=id).first()
+
+    if pitch_id is None:
+         abort(404)
+
+    new_vote = Votes(vote=int(1), user_id=current_user.id, pitches_id=pitch_id.id)
+    new_vote.save_vote()
+    return redirect(url_for('.view_pitch', id=id))
+
+@main.route('/pitch/downvote/<int:id>')
+@login_required
+def downvote(id):
+    '''
+    method to put a downvote
+    '''
+    pitch_id = Pitch.query.filter_by(id=id).first()
+
+    if pitch_id is None:
+         abort(404)
+
+    new_vote = Votes(vote=int(2), user_id=current_user.id, pitches_id=pitch_id.id)
+    new_vote.save_vote()
+    return redirect(url_for('.view_pitch', id=id))
+
+@main.route('/pitch/downvote/<int:id>')
+def vote_count(id):
+    '''
+    method to count the votes
+    '''
+    votes = Votes.query.filter_by(user_id=user_id, line_id=line_id).all()
+
+    total_votes = votes.count()
+
+    return total_votes
