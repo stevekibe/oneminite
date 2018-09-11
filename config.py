f3177c110c1ed76b094f025341bcd845d5ba6ba4
stevekibe <stevekibe2345@gmail.com>
2018-09-11 14:43:22 +0300

    deploy in heroku

--- config.py
+++ config.py
@@ -20,6 +20,7 @@ class DevConfig(config):
     DEBUG = True
 
 config_options = {
+    'production': ProdConfig
     'development': DevConfig
     
 }
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from app.models import User,Pitch,Comments,PitchCategory,Votes
 from flask_migrate import Migrate, MigrateCommand
 
 #app = create_app('development')
-app = create_app('production')
+app = create_app('Production')
 
 manager = Manager(app)
 manager.add_command('server', Server)
